573e89bb25737c8ee1daea3c752829f09f553c89
Shuaib Bapputty <shuaibabh@gmail.com>
2026-05-14 12:55:57 +0530

    udpate parent id of access key

--- keys.tf
+++ keys.tf
@@ -16,7 +16,7 @@ resource "nebius_iam_v1_auth_public_key" "firefly_public_key" {
 
 resource "nebius_iam_v2_access_key" "firefly_access_key" {
   depends_on = [nebius_iam_v1_service_account.firefly_service_account]
-  parent_id  = var.tenant_id
+  parent_id  = var.project_id
   name       = "${local.name_prefix}firefly-access-key${local.name_suffix}"
   account = {
     service_account = {
